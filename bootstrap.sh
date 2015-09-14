a6b2d62699b3138e3ea1c58a2a0761d953f239ea
theus deka <me@theus.be>
2015-09-14 22:27:11 +0200

    Before baking the app

---
+++ bootstrap.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#EOL => LF
+
+echo "\n--- Start provisionning script ---\n"
+
+
+echo "\n--- Install compass ---\n"
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
+gem install compass
+
+
+cd /var/www/public/
+
+echo "\n--- Composer (Load cakePHP, ApertureConnector, ...) ---\n"
+composer update
+
+
+echo "\n--- Bower (Bootstrap, CKEditor, jquery, ...) ---\n"
+bower update
+
+
+echo "\n--- Provisionning done ---\n"
